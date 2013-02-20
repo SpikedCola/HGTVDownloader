6fbd1a4228c6c7d295862cda12ee07dfe028b922
J Skoba <parkinglotlust@gmail.com>
2013-02-19 20:28:55 -0500

    added "finished shows" file so we dont re-check things that have been finished added response checking

--- download.php
+++ download.php
@@ -9,104 +9,138 @@
 	 * 
 	 * Files that exist won't be re-downloaded
 	 * 
+	 * Shows found in $finishedShows won't have their episodes fetched.
+	 * >> NOTE: $finishedShows file must always end in \n (unless empty of course)
+	 * 
 	 * Requires rtmpdump in the same folder as this script.
 	 */	
 	
 	$downloadFolder = 'D:/HGTV/';
 	$timesToRetry = 5;
+	$finishedShows = __DIR__ . '/finished.txt'; // ignore these 
 	
 	echo PHP_EOL;
+	echo 'HGTV Show Downloader v0.1'.PHP_EOL.PHP_EOL;
+	
+	$ignoreShows = array();
+	if ($fp = fopen($finishedShows, 'a+')) {
+		while ($show = trim(fgets($fp))) {
+			if (strpos($show, '#') !== 0) {
+				$ignoreShows[] = $show;
+			}
+		}
+		fclose($fp);
+	}
+	else {
+		echo '>> Note: "Finished Show" file (' . $finishedShows . ') couldn\'t be read. <<'.PHP_EOL.PHP_EOL;
+	}
 	
 	$showListUrl = 'http://feeds.theplatform.com/ps/JSON/PortalService/2.2/getCategoryList?callback=&field=ID&field=depth&field=hasReleases&field=fullTitle&PID=HmHUZlCuIXO_ymAAPiwCpTCNZ3iIF1EG&query=CustomText|PlayerTag|z/HGTVNEWVC%20-%20New%20Video%20Center&field=title&field=fullTitle&customField=TileAd&customField=DisplayTitle';
 	
-	echo 'HGTV Show Downloader v0.1'.PHP_EOL.PHP_EOL;
 	echo '> Loading show list...';
 	
 	$shows = array();
 	$response = file_get_contents($showListUrl);
 	
-	echo ' Parsing...';
-	
-	$json = json_decode($response);
-	foreach ($json->items as $show) {
-		if ($show->depth > 2) {
-			unset($show->customData);
-			$parts = explode('/', $show->fullTitle);
-			if (count($parts) < 4) {
-				print_r($show);
-				throw new Exception('show title didnt have at least 4 parts!');
-			}
-			$show->category = $parts[1];
-			$show->show_title = $parts[2];
-			$show->season = $parts[3];
-			$shows[$show->category][$show->show_title][$show->title][] = $show;
-		}
-	}
-	
-	echo ' Done!'.PHP_EOL.PHP_EOL;
-	echo '== Lets start downloading some shows :) =='.PHP_EOL;
-	
-	if (!is_dir($downloadFolder)) {
-		mkdir($downloadFolder);
-	}
-	
-	foreach ($shows as $category => $serieses) {
-		if (!is_dir($downloadFolder.$category)) {
-			mkdir($downloadFolder.$category);
-		}
-		foreach ($serieses as $series => $seasons) {
-			if (!is_dir($downloadFolder.$category.'/'.$series)) {
-				mkdir($downloadFolder.$category.'/'.$series);
-			}
-			foreach ($seasons as $season => $data) {
-				$folder = $downloadFolder.$category.'/'.$series.'/'.$season.'/';
-				if (!is_dir($folder)) {
-					mkdir($folder);
+	if ($response) {
+		echo ' Parsing...';
+
+		$json = json_decode($response);
+		foreach ($json->items as $show) {
+			if ($show->depth > 2) {
+				unset($show->customData);
+				$parts = explode('/', $show->fullTitle);
+				if (count($parts) < 4) {
+					print_r($show);
+					throw new Exception('show title didnt have at least 4 parts!');
 				}
-				
-				echo PHP_EOL.'Finding episodes for "' . $series . ' - ' . $season . '"...';
-				$episodes = getEpisodes($data[0]->ID);
-				echo ' Found ' . count($episodes) . ' episode(s)'.PHP_EOL;
-				
-				if (count($episodes) > 0) {
-					foreach ($episodes as $episode) {
-						$fileName = str_replace(array(':', '?'), array(' -', ''), $episode['title']);
-						if (file_exists($folder.$fileName.'.flv')) {
-							echo '> "'.$episode['title'].'" exists, skipping.'.PHP_EOL;
+				$show->category = $parts[1];
+				$show->show_title = $parts[2];
+				$show->season = $parts[3];
+				$shows[$show->category][$show->show_title][$show->title][] = $show;
+			}
+		}
+
+		echo ' Done!'.PHP_EOL.PHP_EOL;
+		echo '== Lets start downloading some shows :) =='.PHP_EOL;
+
+		if (!is_dir($downloadFolder)) {
+			mkdir($downloadFolder);
+		}
+
+		foreach ($shows as $category => $serieses) {
+			if (!is_dir($downloadFolder.$category)) {
+				mkdir($downloadFolder.$category);
+			}
+			foreach ($serieses as $series => $seasons) {
+				if (!in_array($series, $ignoreShows)) {
+					if (!is_dir($downloadFolder.$category.'/'.$series)) {
+						mkdir($downloadFolder.$category.'/'.$series);
+					}
+					foreach ($seasons as $season => $data) {
+						$folder = $downloadFolder.$category.'/'.$series.'/'.$season.'/';
+						if (!is_dir($folder)) {
+							mkdir($folder);
 						}
-						else {
-							$tries = 0;
-							while(true) {
-								echo '> Downloading "'.$episode['title'].'"... ';
-								$code = null;
-								$temp = array();
-								exec('rtmpdump -r "' . $episode['stream'] . '" -y "' . $episode['playlist'] . '" -o "' . $folder.$fileName . '.flv"', $temp, $code);
-								if ($code == 0) {	
-									echo ' Download complete!'.PHP_EOL;
-									break;
+
+						echo PHP_EOL.'Finding episodes for "' . $series . ' - ' . $season . '"...';
+						$episodes = getEpisodes($data[0]->ID);
+						echo ' Found ' . count($episodes) . ' episode(s)'.PHP_EOL;
+
+						if (count($episodes) > 0) {
+							foreach ($episodes as $episode) {
+								$fileName = str_replace(array(':', '?'), array(' -', ''), $episode['title']);
+								if (file_exists($folder.$fileName.'.flv')) {
+									echo '> "'.$episode['title'].'" exists, skipping.'.PHP_EOL;
 								}
 								else {
-									echo '> Download failed.';
-									if (file_exists($folder.$fileName.'.flv')) {
-										echo ' Unlinking file...';
-										unlink($folder.$fileName.'.flv'); 
-									}
-									if ($tries < $timesToRetry) {
-										echo ' Trying again.'.PHP_EOL;
-										$tries++;
-									}
-									else {
-										var_dump($episode);
-										echo 'Too many retries';
-										die;
+									$tries = 0;
+									while(true) {
+										echo '> Downloading "'.$episode['title'].'"... ';
+										$code = null;
+										$temp = array();
+										exec('rtmpdump -r "' . $episode['stream'] . '" -y "' . $episode['playlist'] . '" -o "' . $folder.$fileName . '.flv"', $temp, $code);
+										if ($code == 0) {	
+											echo ' Download complete!'.PHP_EOL;
+											break;
+										}
+										else {
+											echo '> Download failed.';
+											if (file_exists($folder.$fileName.'.flv')) {
+												echo ' Unlinking file...';
+												unlink($folder.$fileName.'.flv'); 
+											}
+											if ($tries < $timesToRetry) {
+												echo ' Trying again.'.PHP_EOL;
+												$tries++;
+											}
+											else {
+												var_dump($episode);
+												echo 'Too many retries';
+												die;
+											}
+										}
 									}
 								}
 							}
 						}
 					}
+					if ($fp = fopen($finishedShows, 'a')) {
+						fwrite($fp, $series."\n");
+						fclose($fp);
+					}
+					else {
+						echo '>> Failed saving to "Finished Shows" file ('.$finishedShows.')'.PHP_EOL;
+					}
+				}
+				else {
+					echo PHP_EOL . 'Ignoring "' . $series . '"'.PHP_EOL;
 				}
 			}
 		}
+	}
+	else {
+		throw new Exception('Show list response was empty');
 	}
 	
 	/**
@@ -122,63 +156,73 @@
 		$url = 'http://feeds.theplatform.com/ps/JSON/PortalService/2.2/getReleaseList?callback=&field=ID&field=contentID&field=PID&field=URL&field=categoryIDs&field=length&field=airdate&field=requestCount&PID=HmHUZlCuIXO_ymAAPiwCpTCNZ3iIF1EG&contentCustomField=Show&contentCustomField=Episode&contentCustomField=Network&contentCustomField=Season&contentCustomField=Zone&contentCustomField=Subject&query=Categories|z/HGTVNEWVC%20-%20New%20Video%20Center&param=Site|shaw.hgtv.ca&param=k0|id&param=v0|399&param=k1|cnt&param=v1|lifestylehomes&param=k2|nk&param=v2|sbrdcst&param=k3|pr&param=v3|hgtv&param=k4|kw&param=v4|shaw&param=k5|test&param=v5|test&param=k6|ck&param=v6|video&param=k7|imp&param=v7|video&param=k8|liveinsite&param=v8|hrtbuy&query=CategoryIDs|'.$id.'&field=thumbnailURL&field=title&field=length&field=description&field=assets&contentCustomField=Part&contentCustomField=Clip%20Type&contentCustomField=Web%20Exclusive&contentCustomField=ChapterStartTimes&contentCustomField=AlternateHeading&startIndex=1&endIndex=99&sortField=airdate&sortDescending=true';
 		$episodes = array();
 		$response = file_get_contents($url);
-		$json = json_decode($response);
-		foreach ($json->items as $episode) {
-			$show = null;
-			$ep = null;
-			$season = null;
-			foreach ($episode->contentCustomData as $data) {
-				switch ($data->title) {
-					case 'Show':
-						$show = $data->value;
-						break;
-					case 'Episode':
-						$ep = $data->value;
-						break;
-					case 'Season':
-						$season = $data->value;
-						break;
+		if ($response) {
+			$json = json_decode($response);
+			foreach ($json->items as $episode) {
+				$show = null;
+				$ep = null;
+				$season = null;
+				foreach ($episode->contentCustomData as $data) {
+					switch ($data->title) {
+						case 'Show':
+							$show = $data->value;
+							break;
+						case 'Episode':
+							$ep = $data->value;
+							break;
+						case 'Season':
+							$season = $data->value;
+							break;
+					}
+				}
+				if (!$show || !$ep || !$season) {
+					continue;
+				}
+				$title = $show . ' - S' . $season . 'E' . $ep . ' - ' . $episode->title;
+				$xmlString = file_get_contents($episode->URL);
+				if ($xmlString) {
+					$xml = simplexml_load_string($xmlString);
+					$streamUrl = null;
+					foreach ($xml->choice as $choice) {
+						if (strpos($choice->url, 'rtmp://') !== false) {
+							$streamUrl = $choice->url;
+						}
+					}
+					if (!$streamUrl) {
+						throw new Exception('Failed to find rtmp stream for ' . $id);
+					}
+					$streamParts = explode('<break>', $streamUrl);
+					if (count($streamParts) != 2) {
+						throw new Exception('stream url didnt explode on <break>. url was: ' . $streamUrl);
+					}
+					$stream = $streamParts[0];
+					$playlistFile = $streamParts[1];
+					$playlistFileExtension = pathInfo($playlistFile, PATHINFO_EXTENSION);
+					if ($playlistFileExtension == 'flv') {
+						$playlist = 'flv:'.str_replace('.flv', '', $playlistFile);
+					}
+					elseif ($playlistFileExtension == 'mp4') {
+						$playlist = 'mp4:'.$playlistFile;
+					}
+					else {
+						throw new Exception('Unhandled playlist file extension: ' . $playlistFile);
+					}
+
+					$episodes[] = array(
+					    'title' => $title,
+					    'stream' => $stream,
+					    'playlist' => $playlist
+					);
+				}
+				else {
+					throw new Exception('episode->url response was empty');
 				}
 			}
-			if (!$show || !$ep || !$season) {
-				continue;
-			}
-			$title = $show . ' - S' . $season . 'E' . $ep . ' - ' . $episode->title;
-			$xmlString = file_get_contents($episode->URL);
-			$xml = simplexml_load_string($xmlString);
-			$streamUrl = null;
-			foreach ($xml->choice as $choice) {
-				if (strpos($choice->url, 'rtmp://') !== false) {
-					$streamUrl = $choice->url;
-				}
-			}
-			if (!$streamUrl) {
-				throw new Exception('Failed to find rtmp stream for ' . $id);
-			}
-			$streamParts = explode('<break>', $streamUrl);
-			if (count($streamParts) != 2) {
-				throw new Exception('stream url didnt explode on <break>. url was: ' . $streamUrl);
-			}
-			$stream = $streamParts[0];
-			$playlistFile = $streamParts[1];
-			$playlistFileExtension = pathInfo($playlistFile, PATHINFO_EXTENSION);
-			if ($playlistFileExtension == 'flv') {
-				$playlist = 'flv:'.str_replace('.flv', '', $playlistFile);
-			}
-			elseif ($playlistFileExtension == 'mp4') {
-				$playlist = 'mp4:'.$playlistFile;
-			}
-			else {
-				throw new Exception('Unhandled playlist file extension: ' . $playlistFile);
-			}
-			
-			$episodes[] = array(
-			    'title' => $title,
-			    'stream' => $stream,
-			    'playlist' => $playlist
-			);
+			return $episodes;
 		}
-		return $episodes;
+		else {
+			throw new Exception('Episode list response was empty');
+		}
 	}
 	
 ?>
